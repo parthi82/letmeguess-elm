6a8304f356669b715cd75526e0619a8b7b8689b5
parthibhan kp <parthibhank82@gmail.com>
2017-01-17 18:52:27 +0530

    passing Mix.env to deps()

--- mix.exs
+++ mix.exs
@@ -9,7 +9,7 @@ defmodule Letmeguess.Mixfile do
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps(),
+     deps: deps(Mix.env),
      test_coverage: [tool: ExCoveralls]]
   end
 
