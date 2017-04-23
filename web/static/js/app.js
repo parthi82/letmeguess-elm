19cd02afc3ac495c67c238e14225b3e8d49cd8d5
parthibhan kp <parthibhank82@gmail.com>
2017-04-24 00:06:05 +0530

    fix using secure websocket in heroku

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -1,5 +1,6 @@
 import Elm from '../../elm/Main';
-const { host, pathname, href: roomUrl } = window.location;
+const { protocol, host, pathname, href: roomUrl } = window.location;
 const channel = `room:${pathname.replace('/', '')}`;
-const socketUrl = `ws://${host}/socket/websocket`
+const wsprotocol = protocol === 'https:'? 'wss:' : 'ws:';
+const socketUrl = `${wsprotocol}//${host}/socket/websocket`
 Elm.Main.embed(document.getElementById('elm-app'), {channel, socketUrl, roomUrl});
