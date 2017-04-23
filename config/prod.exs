4f2549c42c6b4d90160f0eaec04e87c06d4d1050
parthibhan kp <parthibhank82@gmail.com>
2017-04-23 23:58:40 +0530

    fix prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,9 @@ use Mix.Config
 # which you typically run after static files are built.
 config :letmeguess, Letmeguess.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
+  url: [host: "letmeguess.herokuapp.com", port: 80],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json"
 
 # Do not print debug messages in production
@@ -62,4 +64,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
