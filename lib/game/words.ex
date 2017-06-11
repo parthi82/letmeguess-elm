37e65493c728aa1c4746330f7748d05cedca9fed
parthibhan kp <parthibhank82@gmail.com>
2017-06-12 00:55:53 +0530

    list of words to use

---
+++ lib/game/words.ex
@@ -0,0 +1,40 @@
+ words = ["cat", "sun", "cup", "ghost", "flower", "pie","cow", "banana", "snowflake",
+"bug", "book", "jar","snake", "light", "tree","lips", "apple", "socks", "necklace"
+"smile", "swing","coat", "shoe", "water","heart", "hat", "ocean","kite", "dog",
+"mouth","milk", "duck", "eyes","skateboard", "bird", "boy", "person", "detergent"
+"girl", "mouse", "ball", "house","star", "nose", "bed", "whale", "jacket", "shirt",
+"hippo", "beach", "egg","face", "cookie", "cheese", "ice cream", "scar" "drum",
+"circle","spoon", "worm", "spider", "web", "jungle", "important", "mime", "stairs"
+"peasant", "baggage", "hail", "clog", "pizza", "sauce", "saucer", "password", "dance"
+"scream", "newsletter", "dripping", "lie", "catalog", "wife", "husband", "kitchen"
+"laser", "myth", "biscuit", "macaroni", "rubber", "darkness", "yolk", "exercise",
+"vegetarian", "", "chestnut", "", "glitter", "neighbor", "dizzy", "shelf"
+"fireside", "retail", "drawback", "logo", "fabric", "mirror", "barber", "jazz",
+"migrate", "drought", "commercial", "bargain", "download", "professor", "bathroom"
+"landscape", "goggles", "vitamin", "rainwater", "astronaut", "champion", "lyrics",
+"treasure", "bridge", "bone", "grapes", "bell", "jellyfish", "bunny", "truck", "money"
+"grass", "door", "monkey", "bread", "ears", "bowl", "bracelet", "alligator", "hoodie"
+"bat", "clock", "lollipop", "moon", "doll", "orange", "ear", "basketball", "wallet"
+"bike","airplane", "pen", "inchworm", "seashell", "rocket", "cloud", "bear", "corn",
+"chicken", "purse", "glasses", "carrot", "turtle", "pencil", "horse", "dinosaur",
+"head", "lamp", "snowman", "ant", "giraffe", "cupcake", "chair", "leaves", "snail",
+"baby", "balloon", "bus", "cherry", "crab", "football", "branch", "robot", "century",
+"vision", "avocado", "island", "birthday", "monk", "pilot", "curtain", "shampoo",
+"dentist", "world", "point", "zoo", "fireman", "half", "lace", "yardstick", "rib",
+"koala", "titanic", "ship", "aladdin", "sandwich", "pizza", "beans", "toaster",
+"soup", "soap", "mushroom", "work", "meatball", "freeze", "noodles", "popcorn",
+"fruit", "phone", "magnet", "calendar", "paint", "dustbin", "button", "piano",
+"toilet", "crayons", "skirt", "garbage", "flowers", "sweater", "camera", "towel",
+"pant", "shirt", "boots", "bicycle", "scissors", "torch", "sofa", "sunglasses",
+"pillow", "scarf", "ladder", "magazine", "headband", "headphone", "jeans", "shoes"
+"newspaper", "iron", "mailbox", "punch", "crib", "laundry", "fan", "lunchbox",
+"candle", "broom", "batteries", "paperclip", "computer", "shovel", "television",
+"river", "egypt", "pyramid", "paris", "map", "spray", "luggage", "airport", "stove"
+"summer", "winter", "spring", "lake", "train", "hiking", "surfboard", "sunscreen",
+"passport", "movies", "party", "bank", "giraffe", "panda", "camel", "dolphin",
+"crocodile", "penguin", "swordfish", "dragon", "kitten", "shark", "rabbit", "bulldog"
+"clownfish", "reindeer", "unicorn", "puppy", "tortoise", "squirrel", "catfish",
+"donkey", "frog", "lion", "tiger", "ape", "sheep", "cricket", "stoplight", "recycle"
+"wristwatch", "windmill", "throne", "bowtie", "headache", "flute", "magic", "coach",
+"claw", "nightmare", "bite", "plumber", "space", "taxi", "tennis", "blanket", "rock"
+"hallowen", "christmas", "hurdle", "warmth", "heels", "suitcase", "coffee", "forest"]
