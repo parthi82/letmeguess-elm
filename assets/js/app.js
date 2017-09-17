d5f5f282eee22530bd8cf1141679d4f6db6ee5ea
parthibhan kp <parthibhank82@gmail.com>
2017-09-17 21:43:44 +0530

    upgrade to phoenix 1.3

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+import Elm from '../elm/Main';
+const { protocol, host, pathname, href: roomUrl } = window.location;
+const channel = `room:${pathname.replace('/', '')}`;
+const wsprotocol = protocol === 'https:'? 'wss:' : 'ws:';
+const socketUrl = `${wsprotocol}//${host}/socket/websocket`
+Elm.Main.embed(document.getElementById('elm-app'), {channel, socketUrl, roomUrl});
---
+++ assets/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path');
+
+module.exports = {
+  entry: [
+    "./js/app.js",
+    "./elm/Main.elm"
+  ],
+  output: {
+    path: path.resolve(__dirname, '../priv/static/js'),
+    filename: "app.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.(css|scss)$/,
+        use: [
+          'style-loader',
+          'css-loader',
+        ]
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+        test: /\.scss$/,
+        loaders: ['style', 'css', 'sass']
+      },
+      {
+          test: /\.jsx?$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['latest'],
+          },
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file'
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml'
+      },
+      {
+        test: /\.elm$/,
+        exclude: [/elm-stuff/, /node_modules/],
+        loader: `elm-webpack-loader?verbose=true&warn=true&debug=true&cwd=${__dirname}/elm`
+      }
+    ],
+    // don't parse Elm files since they won't be using require or define calls
+    noParse: [/\.elm$/]
+  },
+
+  resolve: {
+    extensions: ['*', '.scss', '.css', '.js', '.elm'],
+    modules: ['node_modules'],
+  }
+}
