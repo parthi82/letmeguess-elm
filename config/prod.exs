e2dc181fab3838f123ac14d21f34bdcc770b1588
parthibhan kp <parthibhank82@gmail.com>
2017-09-18 00:47:33 +0530

    update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :letmeguess, Letmeguess.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https" host: "letmeguess.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "letmeguess.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
