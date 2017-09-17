b9d8eb0fe0caf545de6c6c1f2b817999b0d5a8a7
parthibhan kp <parthibhank82@gmail.com>
2017-09-18 00:32:59 +0530

    update cache_static_manifest

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :letmeguess, Letmeguess.Endpoint,
   url: [host: "letmeguess.herokuapp.com", port: 80],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
 config :logger, level: :info
