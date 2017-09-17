ace65374d0f0ce3dd46b3b7c47a7ce2b294e46f9
parthibhan kp <parthibhank82@gmail.com>
2017-09-18 00:46:06 +0530

    update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -14,9 +14,9 @@ use Mix.Config
 config :letmeguess, Letmeguess.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [scheme: "https" host: "letmeguess.herokuapp.com", port: 443],
-  secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
