18c66af8cc1a6f0cd0a06a2e7d102336800e67d1
parthibhan kp <parthibhank82@gmail.com>
2017-09-18 00:44:42 +0530

    update prod.exs and user_socket.ex

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :letmeguess, Letmeguess.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "letmeguess.herokuapp.com", port: 80],
+  url: [scheme: "https" host: "letmeguess.herokuapp.com", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
