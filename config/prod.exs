759d4ba1cabd2adea2aaa27c3bca93a0a2703fa5
parthibhan kp <parthibhank82@gmail.com>
2017-09-18 10:14:29 +0530

    update

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :letmeguess, Letmeguess.Endpoint,
   url: [scheme: "https", host: "letmeguess.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
-  secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
+  secret_key_base: 'SECRET_KEY_BASE'
 
 # Do not print debug messages in production
 config :logger, level: :info
